415b09cb7a89bc533c9c7684ab3734a5c47fa947
LohanMout <159461731+LohanMout@users.noreply.github.com>
2024-05-10 14:00:56 +0000

    drag and drop fonctionnel

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -22,37 +22,73 @@ export default function Dossier({
   // État du formulaire de modification
   const [frmDossierOuvert, setFrmDossierOuvert] = useState(false);
 
+  //etat du dossier ouvert
+  const [contenuDossierVisible, setContenuDossierVisible] = useState(false);
+
   // Style dynamique de la couleur du dossier.
   let objStyle = {
     backgroundColor: couleur
   }
+
+  //gerer drag and drop
+  function gererDragEnter(evt){
+    evt.dataTransfer.effectAllowed = "link";
+    evt.preventDefault();
+  }
+
+  function gererDrop(evt){
+    const url = evt.dataTransfer.getData('URL');
+    evt.preventDefault();
+    //ajouter le signet dans firestore
+    console.log("url du signet deposer : " + url);
+    setContenuDossierVisible(true);
+  }
+
+  function gererDragOver(evt){
+    evt.preventDefault();
+  }
+
+  function gererDragLeave(){
+    return;
+  }
+
   return (
-    <article className="Dossier">
-      <div className="couverture">
-        <IconButton className='btn-dossier tourner' color='primary'>
-          <ThreeSixtyIcon/>
-        </IconButton>
-        <img src={couverture || couvertureDefaut} alt={titre}/>
-        <IconButton 
-          className='btn-dossier supprimer' 
-          color='secondary'
-          onClick={()=>supprimer(id)}
-        >
-          <DeleteIcon/>
-        </IconButton>
-      </div>
-      <div className="info" style={objStyle}>
-        <h2>{titre}</h2>
-        <p>Modifié : {dateModif}</p>
-        <FrmDossier
-          ouvert={frmDossierOuvert}
-          setOuvert={setFrmDossierOuvert}
-          actionDossier={modifier}
-          dossierPrec={{id, titre, couverture, couleur}}
-        />
-        <IconButton onClick={()=>setFrmDossierOuvert(true)} className='btn-dossier modifier' color='tertiary'>
-          <EditIcon/>
-        </IconButton>
+    <article className={`Dossier ${contenuDossierVisible && 'actif'}`} style={objStyle} onDragEnter={gererDragEnter} onDrop={gererDrop} onDragOver={gererDragOver} onDragLeave={gererDragLeave}>
+      <div className="carte">
+        <div className='endroit'>
+          <div className="couverture">
+          <IconButton className='btn-dossier tourner' color='primary' onClick={() => setContenuDossierVisible(true)}>
+            <ThreeSixtyIcon/>
+          </IconButton>
+          <img src={couverture || couvertureDefaut} alt={titre}/>
+          <IconButton
+            className='btn-dossier supprimer'
+            color='secondary'
+            onClick={()=>supprimer(id)}
+          >
+            <DeleteIcon/>
+          </IconButton>
+          </div>
+          <div className="info" >
+          <h2>{titre}</h2>
+          <p>Modifié : {dateModif}</p>
+          <FrmDossier
+            ouvert={frmDossierOuvert}
+            setOuvert={setFrmDossierOuvert}
+            actionDossier={modifier}
+            dossierPrec={{id, titre, couverture, couleur}}
+          />
+          <IconButton onClick={()=>setFrmDossierOuvert(true)} className='btn-dossier modifier' color='tertiary'>
+            <EditIcon/>
+          </IconButton>
+          </div>
+        </div>
+        <div className="envers">
+          <IconButton className='btn-dossier tourner' color='primary' onClick={() => setContenuDossierVisible(false)}>
+            <ThreeSixtyIcon/>
+          </IconButton>
+          <a target='_blank' href="https://www.speedtest.net/">speedtest ookla</a>
+        </div>
       </div>
     </article>
   );
